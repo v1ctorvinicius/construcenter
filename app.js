d6ca5fe39f0ce29976f962fc31a7b87dc779bb62
v1ctorvinicius <victorviniciuspj@gmail.com>
2025-08-16 21:06:56 -0300

    feat: add hero videos playlist

---
+++ app.js
@@ -0,0 +1,22 @@
+// Lista de vídeos para o hero
+const videos = [
+  "img/hero-video-0.mp4",
+  "img/hero-video-1.mp4",
+  
+];
+
+const videoElement = document.getElementById("bg-video");
+let currentVideo = 0;
+
+function playNextVideo() {
+  videoElement.src = videos[currentVideo];
+  videoElement.play();
+
+  videoElement.onended = function () {
+    currentVideo = (currentVideo + 1) % videos.length; // volta ao início quando terminar
+    playNextVideo();
+  };
+}
+
+// inicia a rotação quando a página carregar
+window.addEventListener("DOMContentLoaded", playNextVideo);
