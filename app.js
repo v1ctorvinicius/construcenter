80eebb67314bd46e4e33d4d2e5d9db40df9168d7
v1ctorvinicius <victorviniciuspj@gmail.com>
2025-08-18 12:07:29 -0300

    fix(videos): add optimized videos pt 2

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // Lista de vídeos para o hero
 const videos = [
-  "img/hero-video-3.mp4",
-  "img/hero-video-4.mp4",
-  "img/hero-video-5.mp4",
-  "img/hero-video-1.mp4",
-  "img/hero-video-0.mp4",
-  "img/hero-video-2.mp4",
+  "img/hero-video-3-opt.mp4",
+  "img/hero-video-4-opt.mp4",
+  "img/hero-video-5-opt.mp4",
+  "img/hero-video-1-opt.mp4",
+  "img/hero-video-0-opt.mp4",
+  "img/hero-video-2-opt.mp4",
 ];
 
 const videoElement = document.getElementById("bg-video");
@@ -27,6 +27,7 @@ function playNextVideo() {
     // quando estiver pronto, troca sem dar tela preta
     nextVideo.addEventListener("canplaythrough", () => {
       videoElement.src = nextVideo.src;
+      videoElement.currentTime = 0;
       videoElement.play();
     });
   };
